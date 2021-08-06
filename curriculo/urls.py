38f5495eacbbc909fe621e6bb1e475a13158838d
Gabriel <65578956+gab98fra@users.noreply.github.com>
2021-08-06 18:54:54 -0500

    Update urls.py

--- curriculo/urls.py
+++ curriculo/urls.py
@@ -3,6 +3,9 @@ from django.contrib.auth.decorators import login_required
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import handler404, handler500
+from home.views import Error404View, Error500View
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,3 +18,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    
+handler404 = Error404View.as_view()
+handler500 = Error500View.as_error_view()
