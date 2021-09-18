9764e09fdd061f47d80a8137796f0773a8f0f77c
Gabriel <65578956+gab98fra@users.noreply.github.com>
2021-09-18 11:01:34 -0500

    Update admin.py

--- account/admin.py
+++ account/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
 
-#Permisos
+#Permissions
 admin.site.register(Permission)
 admin.site.register(ContentType)
 
