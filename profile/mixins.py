4c749b623bec461f3e801cb448e7b21bfc406974
Gabriel <65578956+gab98fra@users.noreply.github.com>
2021-09-19 00:14:09 -0500

    Update mixins.py

--- profile/mixins.py
+++ profile/mixins.py
@@ -4,11 +4,8 @@ from django.urls.base import reverse_lazy
 from dashboard.models import DatosPersonalesModel, DatosContactoModel, ObjetivoProfesionalModel
 
 class Autenticar_ValidarDatosMixin(object):
-    """Verifica:
-        -Que el usuarios haya autenticado
-        -Tenga datos capurados en los modelos: DatosPersonales, DatosContacto y ObjetivosProfesionales
-            -En caso de tenerlo se envía un mensaje y es capturado desde template: dashboard/index con JavaScript
-    """
+    "Authenticated Mixin"
+    
     def dispatch(self, request, *args, **kwargs):
         
         if request.user.is_authenticated:
@@ -26,14 +23,8 @@ class Autenticar_ValidarDatosMixin(object):
 
 
 class CvFilePermissionMixin(object):
-    """Valida que tenga permisos al modelo CvFileModel
-        -get_perms: valida y retorna una tupla
-        -ger_url_redirect:obtiene url de redirección
-        -dispatch: se ejecuta al principo
-            -si tiene los permisos continua con la ejecución
-            -en otro caso se redirecciona al dashboard
-    """
-    #tipodecontenido(app).permiso
+    "Permission Mixin"
+    
     permission_required = ('dashboard.view_cvfilemodel', 'dashboard.change_cvfilemodel',
                            'dashboard.delete_cvfilemodel','dashboard.add_cvfilemodel')
     
